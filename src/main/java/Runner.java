2e6dda7f1a29c27dd1c5b0f70beec85b0f7ab5fb
Brojandos <Brojandos@gmail.com>
2017-02-18 20:53:58 +0600

    Initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+/**
+ * @author: Brojandos.
+ * @creation_date: 18.02.2017.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
